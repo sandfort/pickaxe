a20a165f9419570adedd0dc567ceac7e31f0e3c2
John Sandfort <garnavis@gmail.com>
2014-08-07 19:26:42 -0400

    Adding concurrency examples

---
+++ tut_fibers/fibrous_word_count.rb
@@ -0,0 +1,18 @@
+# Fiber.new takes a block
+# This block is not executed immediately
+words = Fiber.new do
+  File.foreach("testfile") do |line|
+    line.scan(/\w+/) do |word|
+      Fiber.yield word.downcase
+    end
+  end
+  nil
+end
+
+counts = Hash.new(0)
+while word = words.resume
+  counts[word] += 1
+end
+
+counts.keys.sort.each {|k| print "#{k}:#{counts[k]} "}
+
