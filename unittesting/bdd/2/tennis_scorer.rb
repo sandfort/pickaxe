423812c017bfbe162fcd13ffc9de4d2b1af69804
John Sandfort <garnavis@gmail.com>
2014-08-18 15:33:26 -0400

    Adding RSpec behavior-driven development examples

---
+++ unittesting/bdd/2/tennis_scorer.rb
@@ -0,0 +1,6 @@
+class TennisScorer
+  def score
+    "0-0"
+  end
+end
+
---
+++ unittesting/bdd/5/ts_spec.rb
@@ -0,0 +1,28 @@
+require_relative "tennis_scorer"
+
+describe TennisScorer, "basic scoring" do
+
+  let(:ts) { TennisScorer.new }
+
+  it "should start with a score of 0-0" do
+    ts.score.should == "0-0"
+  end
+
+  it "should be 15-0 if the server wins a point" do
+    ts.give_point_to(:server)
+    ts.score.should == "15-0"
+  end
+
+  it "should be 0-15 if the receiver wins a point" do
+    ts.give_point_to(:receiver)
+    ts.score.should == "0-15"
+  end
+
+  it "should be 15-15 after they both win a point" do
+    ts.give_point_to(:receiver)
+    ts.give_point_to(:server)
+    ts.score.should == "15-15"
+  end
+
+end
+
